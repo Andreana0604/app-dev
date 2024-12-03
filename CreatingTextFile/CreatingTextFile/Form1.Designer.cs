40edbf0881b23f0ad8294b438e2fd0dcb3a4203f
Andreana0604 <andreanapaghubasan@gmail.com>
2024-12-03 17:03:19 +0800

    Add files via upload

---
+++ CreatingTextFile/CreatingTextFile/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CreatingTextFile
+{
+    partial class frmLab1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(30, 29);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(20);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(505, 241);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnCreate.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCreate.Location = new System.Drawing.Point(227, 293);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(110, 34);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // frmLab1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(564, 350);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtInput);
+            this.Name = "frmLab1";
+            this.Text = "FrmLab1";
+            this.Load += new System.EventHandler(this.frmLab1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ CreatingTextFile/CreatingTextFile/FrmRegistration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreatingTextFile
+{
+    public partial class FrmRegistration : Form
+    {
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e){}
+
+        private void FrmRegistration_Load(object sender, EventArgs e) 
+        {
+            string[] ListOfProgram = new string[]
+            {
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS in Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management"
+            };
+
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+
+            cbGender.Items.Add("Female");
+            cbGender.Items.Add("Male");
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string getStuNum = txtStudentNo.Text;
+            string getLName = txtLName.Text;
+            string getFName = txtFName.Text;
+            string getMidInitial = txtMI.Text;
+            string getAge = txtAge.Text;    
+            string getGender = cbGender.Text;
+            string getContactNo = txtContactNo.Text;
+            string getBirthday = dtpBirthday.Text;
+            string getPrograms = cbPrograms.Text;
+
+            string newDocName = FrmFileName.SetFileName = txtStudentNo.Text + ".txt";
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, FrmFileName.SetFileName)))
+            {
+                outputFile.WriteLine("Student No: " + getStuNum);
+                outputFile.WriteLine("Last Name: " + getLName);
+                outputFile.WriteLine("First Name: " + getFName);
+                outputFile.WriteLine("Middle Initial: " + getMidInitial);
+                outputFile.WriteLine("Age: " + getAge);
+                outputFile.WriteLine("Gender: " + getGender);
+                outputFile.WriteLine("Contact No: " + getContactNo);
+                outputFile.WriteLine("Birthday: " + getBirthday);
+                outputFile.WriteLine("Program: " + getPrograms);
+                Console.WriteLine(getStuNum, getLName, getFName, getMidInitial, getAge,
+                                  getGender, getContactNo, getBirthday, getPrograms);
+            }
+        }
+
+        private void btnRecords_Click(object sender, EventArgs e)
+        {
+            FrmStudentRecord studentRecordForm = new FrmStudentRecord();
+            studentRecordForm.Show();
+            this.Hide();
+        }
+    }
+}
